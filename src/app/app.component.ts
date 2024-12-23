19d0be1d8106b0d8d073301fc4fc5cbd48a9aad5
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-12-23 15:12:23 +0100

    add validators for checking errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,65 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  passwordMatche = false;
   mySuperForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl('')
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+
+      Validators.pattern(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,}$).+'
+      ),
+    ]),
+    confirmPassword: new FormControl('', Validators.required),
   });
 
+  nameError() {
+    const nameControl = this.mySuperForm.get('name');
+    if (!nameControl) return false;
+
+    return (
+      nameControl.touched &&
+      (nameControl.hasError('required') || nameControl.hasError('minlength'))
+    );
+  }
+
+  emailError() {
+    const emailControl = this.mySuperForm.get('email');
+    if (!emailControl) return false;
+
+    return (
+      emailControl.touched &&
+      (emailControl.hasError('required') || emailControl.hasError('email'))
+    );
+  }
+
   mySuperFunctionSubmit() {
+    this.passwordMatche = this.mySuperForm.value.password === this.mySuperForm.value.confirmPassword;
+    if (!this.passwordMatche) {
+      console.log('🚀 Passwords do not match! 🔥');
+      return false;
+    }
+    if (this.mySuperForm.invalid) {
+      console.log('🚀 Form invalid! 🔥');
+      return;
+    }
     console.log('🚀 Form submitted! 🔥');
     console.log(this.mySuperForm.value);
+    return;
   }
 }
